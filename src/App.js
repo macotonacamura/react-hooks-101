756bbacef298ea0b0a5a3997f1ed16eb83f3ffe4
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2021-07-10 23:09:39 +0900

    implement object state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, {useState} from 'react'
 
 const App = props => {
+//useStateは個々の値だけじゃなく、オブジェクトを持つこともできる
+  const [state, setState] = useState(props)
+  const { name, price } = state //state.priceと書かなくて良くなる (分割代入)
+  // const [name, setName] = useState(props.name) いらなくなる
+  // const [price, setPrice] = useState(props.price) いらなくなる
 
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
-
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
-
-//e.target.value >>> onChangeが発生した時のinputに入力されている文字列
+  // const reset = () => setState(props) //setState(props)だけで、上2行の役割を果たす ⇨ 関数自体短いからonClick={}内に処理を書いちゃう
+    // setPrice(props.price) いらない
+    // setName(props.name)　いらない
 
   return ( //onClick={この中に関数を書いてもOK}
     <>
-      <p>現在の{name}は{price}円です</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={e => setName(e.target.value)}/>
+      <p>現在の{state.name}は{state.price}円です</p>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input value={name} onChange={e => setState({...state, name: e.target.value})}/> //setStateの引数はオブジェクトだから、{}で囲う。...stateでstateを展開して、nameをアップデート。
     </>
   )
 }
@@ -29,3 +28,4 @@ App.defaultProps = {
 }
 
 export default App;
+
