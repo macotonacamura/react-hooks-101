be0def7328850400da553f7e7fdfe138a8e2909c
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2021-07-11 10:26:49 +0900

    create-reducer-for-events

---
+++ src/reducers/index.js
@@ -0,0 +1,68 @@
+//ACTIONにこういうデータが渡ってきたら、、、
+//action = {
+// 	type: 'CREATE_EVENT',
+	// title: '東京オリンピック開催2020',
+	//body: '2021年に延期します。'
+// }
+
+
+//Before stateが空の場合
+
+// state = []
+
+//After どうなればいいのか
+
+// state = [
+//   {
+// 	id : 1,
+//  title: '東京オリンピック開催2020',
+//	body: '2021年に延期します。'
+// }
+// ]
+
+//Before 既にデータが入っている状態の時
+
+// state = [
+// 	{id: 1,title: 'title1', body:'body1'},
+// 	{id: 2,title: 'title2', body:'body2'},
+// 	{id: 3,title: 'title3', body:'body3'},
+// ]
+
+// Afeter どうなればいいのか
+// state = [
+// 	{id: 1,title: 'title1', body:'body1'},
+// 	{id: 2,title: 'title2', body:'body2'},
+// 	{id: 3,title: 'title3', body:'body3'},
+//  {id: 4,title: '東京オリンピック開催2020',body: '2021年に延期します。}
+// ]
+
+//idに注目 前のidが３だから次は４。
+// 状態として存在する最後の要素のidが3だからプラス1して4にしてあげる
+
+
+const events = (state = [], action) => { //イベントを管理する関数定義
+	switch(action.type){
+		case 'CREATE_EVENT':
+		const event = { title: action.title, body: action.body }
+		const length = state.length //ここでstateの数を調べるために、要素の長さを調べてあげる必要がある[]なのか[色々入ってる]のか。idのため
+		const id = length === 0 ? 1 : state[length -1 ].id + 1 //このlength - 1は-1することによって最後の要素を取り出すことができる
+			return [...state, {id, ...event}] // [...stateで今あるstateを展開、{要素を追加}]
+		case 'DELETE_EVENT':
+			return state
+		case 'DELETE_ALL_EVENT':
+			return []
+		default:
+			return state
+	}
+}
+
+export default events //コンポーネントが利用からexportする
+
+
+// if(length === 0){
+// 	id = 1
+// } else {
+// 	id = state[length -1 ].id + 1 //このlength - 1は-1することによって最後の要素を取り出すことができる
+// }
+// return state
+// }
