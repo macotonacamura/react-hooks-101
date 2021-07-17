10d01a634b955dda625df648dac9ee7f3302f215
macotonacamura <65491593+macotonacamura@users.noreply.github.com>
2021-07-17 23:04:03 +0900

    solve prop drilling issue

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,21 @@ import React,{ useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import EventForm from './EventForm'
-// import Event from './Event'
 import Events from './Events'
+import AppContext from '../contexts/AppContext'
 import reducer from '../reducers'
+
 
 
 const App = () => {
 	const [state, dispatch] = useReducer(reducer, []) //reducerで扱うものはイベント一覧[] //状態遷移をさせたいタイミング(onClick{addEvent})でdispatchを呼んでアクションを渡してあげる
   return (
-    <div className="container-fluid">
-    	<EventForm state={state} dispatch={dispatch}/>
-    	<Events state={state} dispatch={dispatch}/>
-    </div>
+  	<AppContext.Provider value={{state, dispatch}}>
+	    <div className="container-fluid">
+	    	<EventForm />
+	    	<Events />
+	    </div>
+	</AppContext.Provider>
   );
 }
 
